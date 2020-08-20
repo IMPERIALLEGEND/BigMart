50759f5270d49592cced95311983534e8d533437
IMPERIALLEGEND <tejaswawadekar@gmail.com>
2020-08-20 18:11:44 +0530

    adding side navbar

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 //js file
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.body.style.backgroundColor = "white";
+  }
