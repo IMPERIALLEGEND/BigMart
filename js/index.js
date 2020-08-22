938bbce929d72bd3e2146e582e5a730912827148
satyam1209 <truecoder24@gmail.com>
2020-08-23 02:34:41 +0530

    fixed slide-nav-btn

--- js/index.js
+++ js/index.js
@@ -2,11 +2,13 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("main").style.visibility = "hidden";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.5)";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("main").style.visibility = "visible";
     document.body.style.backgroundColor = "white";
   }
